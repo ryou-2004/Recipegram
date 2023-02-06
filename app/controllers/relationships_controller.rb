fb91b7e8998dff4c3847204c58c118686d88474f
ryoOkabayashi <>
2023-02-06 15:52:12 +0900

    フォローフォロワー一覧画面のタブによる切り替え実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,11 +12,13 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
+    @user = user
     @users = user.followings
   end
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
+    @user = user
     @users = user.followers
   end
 end
