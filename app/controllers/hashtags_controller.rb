7152e1c6bcea5e86e295908ff26937e6ac9b78a0
ryoOkabayashi <>
2023-03-14 17:59:36 +0900

    ログ削除

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,6 +2,5 @@ class HashtagsController < ApplicationController
   def show
     hashtag = Hashtag.find(params[:id])
     @recipes = Recipe.joins(:recipehashtags).where(recipehashtags: { hashtag_id: hashtag.id })
-    logger.debug("レシピの数は#{@recipes[0].title}")
   end
 end
