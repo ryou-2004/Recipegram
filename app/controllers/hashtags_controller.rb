7ce91a69253630af9adc64c311f10dee54a33f6a
ryoOkabayashi <>
2023-03-16 17:39:20 +0900

    検索失敗時に検索ワードを表示するように変更

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -5,11 +5,10 @@ class HashtagsController < ApplicationController
       @hashtag_name = @hashtag.name
       @recipes = Recipe.joins(:recipehashtags).where(recipehashtags: { hashtag_id: @hashtag.id })
     else
-      @hashtag_name = ''
+      @hashtag_name = params[:hashtag]
     end
   end
   def search
-    logger.debug("params[:searchtag]#{params[:searchtag]}")
     redirect_to action: "show", hashtag: params[:searchtag]
   end
 end
