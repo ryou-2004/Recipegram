60de9e4aedf2a8267522a380acfc8d9cb24b8d8a
ryoOkabayashi <>
2023-03-16 13:17:09 +0900

    検索のレシピ表示をタイムラインと同じに変更

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,6 @@
 class HashtagsController < ApplicationController
   def show
-    hashtag = Hashtag.find(params[:id])
-    @recipes = Recipe.joins(:recipehashtags).where(recipehashtags: { hashtag_id: hashtag.id })
+    @hashtag = Hashtag.find(params[:id])
+    @recipes = Recipe.joins(:recipehashtags).where(recipehashtags: { hashtag_id: @hashtag.id })
   end
 end
