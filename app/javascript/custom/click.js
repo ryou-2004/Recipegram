707b0f1ff9206b057e40424cecd2a381687c5a4e
ryoOkabayashi <>
2023-03-22 16:51:50 +0900

    選択中のタブの下にバーを追加

--- app/javascript/custom/click.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('.tab').on('click',function(e){
-    $('.tab_active').removeClass('tab_active');
-    $(this).addClass('tab_active');
-    $('.box_show').removeClass('box_show');
-    const index = $(this).index();
-    $('.tabbox').eq(index).addClass('box_show');
-  });
-});
