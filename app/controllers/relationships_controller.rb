4c6b5234bd0573d44b0f0273e82a59161043f3e6
ryoOkabayashi <>
2023-03-22 14:39:32 +0900

    フォローされた際のエラー修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,8 @@
 class RelationshipsController < ApplicationController
   # フォローするとき
   def create
-    user = current_user.follow(params[:user_id])
-    user.create_notification_follow!(current_user)
+    relationship = current_user.follow(params[:user_id])
+    relationship.followed.create_notification_follow!(current_user)
     redirect_to request.referer
   end
 
