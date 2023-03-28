45c0eb74f8b7d415ed6b0c30db66b1a2b7b42b35
ryoOkabayashi <>
2023-03-28 17:57:53 +0900

    ログインしていない時に通知画面へ行ってもエラーを起こさないように2

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attribute(:checked, true)
+    if current_user != nil
+      @notifications = current_user.passive_notifications
+      @notifications.where(checked: false).each do |notification|
+        notification.update_attribute(:checked, true)
+      end
     end
   end
 end
