6ae571306f943b281aa22c174455faf2c0ca9b30
ryoOkabayashi <>
2023-03-28 16:23:22 +0900

    レシピの重複表示を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @recipes = @user.recipes.reject { |recipe| recipe == @user.fixed_recipe }
   end
 
   def edit
